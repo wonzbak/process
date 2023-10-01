691df564486be2ab4f897c295a208b50772078ac
won <wzbktek@pm.me>
2023-10-01 15:48:31 +0200

    Dataprovider: add base exception

---
+++ src/DataProvider/DataProviderException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\DataProvider;
+
+class DataProviderException extends \Exception
+{
+
+}
--- src/DataProvider/FileLineDataProvider.php
+++ src/DataProvider/FileLineDataProvider.php
@@ -2,7 +2,27 @@
 
 namespace App\DataProvider;
 
-class FileLineDataProvider
+class FileLineDataProvider extends DataProviderInterface
 {
+    public function provide(mixed $filename): iterable
+    {
+        try {
+            $file = new \SplFileObject($filename);
 
+        } catch (\Exception $e) {
+            throw new DataProviderException("Failed to open file $filename", null, $e);
+
+        }
+        $file->setFlags(\SplFileObject::DROP_NEW_LINE);
+
+        if (!$file->isReadable()) {
+            throw new DataProviderException("File $filename is not readable");
+        }
+
+        while (!$file->eof()) {
+            yield $file->fgets();
+        }
+
+        $file = null;
+    }
 }
